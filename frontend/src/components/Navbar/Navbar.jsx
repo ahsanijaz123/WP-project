de0d9d2836f796b155afcede7dcb73d88aa88d93
Ahsanijaz1234 <ahsanijazadm@gmail.com>
2025-01-05 12:50:47 +0500

    Ahsan Ijaz

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = ({setShowLogin}) => {
         <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
+        {/* <button onClick={()=>setShowLogin(true)}>sign in</button> */}
         <button onClick={()=>setShowLogin(true)}>sign in</button>
       </div>
     </div>

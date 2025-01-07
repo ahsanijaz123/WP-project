ed0731ad0f0ff7842ec3361f45186cc07ac9c798
Ammar Ahmad <23pwbcs0988@uetpeshawar.edu.pk>
2025-01-07 17:41:29 +0500

    Ammar Ahmad

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
-import { Link }from 'react-router-dom'
+import { Link,useNavigate }from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext';
 import { useContext } from "react";
 
 const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("home");
-  const {getTotalCartAmount} =useContext(StoreContext);
+
+  const {getTotalCartAmount,token,setToken} =useContext(StoreContext);
+
+const navigate = useNavigate();
+
+  const logout = () =>{
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/")
+  }
 
   return (
     <div className="navbar">
@@ -27,11 +36,20 @@ const Navbar = ({setShowLogin}) => {
         <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
-        {/* <button onClick={()=>setShowLogin(true)}>sign in</button> */}
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
+
+        {!token?<button onClick={()=>setShowLogin(true)}>sign in</button>
+        :<div className='navbar-profile'>
+          <img src={assets.profile_icon} alt="" />
+          <ul className="nav-profile-dropdown">
+            <li><img src={assets.bag_icon} alt=""/><p>Orders</p></li>
+            <hr />
+            <li onClick={logout}><img src={assets.logout_icon} alt=""/><p>Logout</p></li>
+          </ul>
+        </div>
+        }
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,19 +1,33 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from 'axios'
+
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const [cartItems, setCartItems] = useState({});
-  const addToCart = (itemId) => {
+
+  const [cartItems, setCartItems] = useState({});  
+  const url = "http://localhost:4000"
+  const [token,setToken] = useState("");
+  const [food_list,setFoodlist] = useState([])
+
+
+  const addToCart =async (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
     } else {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
-  };
-  const removeFromCart = (itemId) => {
+
+    if (token) {
+      await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+    }
+  }
+  const removeFromCart = async (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-  };
+    if (token) {
+      await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+    }
+  }
 
  const getTotalCartAmount = () =>{
   let totalAmount = 0;
@@ -26,20 +40,39 @@ const StoreContextProvider = (props) => {
  }
   return totalAmount;
 }
-  
+
+const fetchFoodList = async () => {
+  const response = await axios.get(url+"/api/food/list");
+  setFoodlist(response.data.data)
+}
+
+useEffect(()=>{
+    async function loadData() {
+      await fetchFoodList();
+       if (localStorage.getItem("token")) {
+         setToken(localStorage.getItem("token"));
+       }
+    }
+    loadData();
+},[]) 
+
+
   const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addToCart,
     removeFromCart,
-    getTotalCartAmount
-  };
+    getTotalCartAmount,
+    url,
+    token,
+    setToken
+  }
   return (
     <StoreContext.Provider value={contextValue}>
       {props.children}
     </StoreContext.Provider>
-  );
-};
+  )
+}
 
-export default StoreContextProvider;
+export default StoreContextProvider
